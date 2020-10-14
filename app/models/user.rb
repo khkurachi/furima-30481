9d9e730cc977c32242dc95f3e9aa4579ba52b683
khkurachi <71420589+khkurachi@users.noreply.github.com>
2020-10-14 11:15:16 +0900

    新規登録の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '漢字を使用してください' } do
+    validates :f_name_kanji
+    validates :l_name_kanji
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'カナ文字を使用してください' } do
+    validates :f_name_kana
+    validates :l_name_kana
+  end
+  validates :birthday, presence: true
 end
