b7d77fc30d7847bf807df637e2790d76acd7169b
khkurachi <71420589+khkurachi@users.noreply.github.com>
2020-10-18 11:31:39 +0900

    before_actionの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :sign_in, :log_in, :show]
-  before_action :set_item,only: [:show, :edit]
+  before_action :set_item,only: [:show, :edit, :update]
   before_action :move_to_index, only: :edit
   def index
     @items = Item.all.order("created_at DESC")
@@ -27,7 +27,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id]) 
     if @item.update(item_params)
         redirect_to item_path(@item.id)
       else
