88e6da908ae02588064a1ceefe7dfd0039c79c0a
khkurachi <71420589+khkurachi@users.noreply.github.com>
2020-10-18 10:21:12 +0900

    first commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,19 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
+    item = Item.find(params[:id]) 
+    if item.update(item_params)
+        redirect_to item_path(item.id), method: :get
+      else
+        render :edit
+      end
   end
 
   def destroy
