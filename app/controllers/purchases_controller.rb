7beb2f88a8a31f7d93beb82048dbf447bb621d00
khkurachi <71420589+khkurachi@users.noreply.github.com>
2020-10-19 15:51:20 +0900

    バリデーションの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,7 +6,21 @@ class PurchasesController < ApplicationController
     @purchases = Purchase.new
   end
 
+  def create
+    @purchase = Purchase.create(purchase_params)
+    if @purchase.valid?
+      @purchase.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
   private
+
+  def purchase_params
+    params.require(:purchase).permit(:price)
+  end
 
   def set_purchase
     @item = Item.find(params[:item_id])
