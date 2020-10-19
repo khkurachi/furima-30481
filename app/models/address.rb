533bfd29157eec8b3e543b613d815ae6dc241ca2
khkurachi <71420589+khkurachi@users.noreply.github.com>
2020-10-19 13:26:34 +0900

    バリデーションの追加

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :purchase
   belongs_to_active_hash :prefecture
+
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 } 
+  with_options presence: true do
+    validates :postal_code
+    validates :municipality
+    validates :address
+    validates :phone_number
+  end
 end
