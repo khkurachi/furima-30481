2891f9550e2bdd1f247c641b898e576b4aada6ab
khkurachi <71420589+khkurachi@users.noreply.github.com>
2020-10-19 13:00:58 +0900

    before_actionの追加
    ログアウトユーザーは購入画面に進もうとするとログイン画面へ飛ばされる
    出品者が自分で購入しようとするとトップページに飛ばされる

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,8 +1,25 @@
 class PurchasesController < ApplicationController
-
+  before_action :set_purchase, only: :index
+  before_action :move_to_log_in, only: :index
+  before_action :move_to_top, only: :index
   def index
     @purchases = Purchase.new
-    @item = Item.find(params[:item_id])
+  end
 
+  private
+
+  def set_purchase
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_log_in
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    end
+  end
+  def move_to_top
+    if user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
