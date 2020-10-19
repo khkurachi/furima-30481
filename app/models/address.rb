57dc4e27c5bd47150d0f69d88f1e46e155305695
khkurachi <71420589+khkurachi@users.noreply.github.com>
2020-10-19 22:21:42 +0900

    バリデーションの修正
    presence: trueを一つにまとめる

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,11 +2,11 @@ class Address < ApplicationRecord
   belongs_to :purchase
   belongs_to_active_hash :prefecture
 
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 } 
   with_options presence: true do
-    validates :postal_code
-    validates :municipality
+    validates :prefecture_id, numericality: { other_than: 1 } 
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/ }  
+    validates :munipality, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
     validates :address
-    validates :phone_number
+    validates :phone_number, format: {with: /\A\d{10,11}\z/ }
   end
 end
